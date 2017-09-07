e35b5b65c0b6471a27a639b2d9c7397002091450
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-09-07 13:49:55 -0400

    Header done. Hash symbol must be changed to menu icon

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import './styles.css';
+
 class Header extends Component {
   render() {
     return (
-      <div> 
-        <h1>Header</h1>
+      <div className='header'> 
+        <div className='logo'>
+          <h1 className='p-txt'>Bonjour Ana</h1>
+        </div>
+        <div className='menu'>
+          <h1>#</h1>
+        </div>
       </div>
     ); 
   }
