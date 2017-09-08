1e84ce950f79e608876525e1b3546174b34a9759
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-09-08 14:45:08 -0400

    Footer top css added

--- front-end/src/components/Footer/FooterTop.js
+++ front-end/src/components/Footer/FooterTop.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 
+import facebook from './images/facebook-icon.svg';
+import twitter from './images/twitter-icon.svg';
+import instagram from './images/instagram-icon.svg';
+
 import './styles.css';
 
 class FooterTop extends Component {
   render() {
     return (
-      <div className='footer'> 
+      <div className='footerTop'> 
+        <div className='social'>
+          <img src={ facebook } />
+          <img src={ twitter } />
+          <img src={ instagram } />
+        </div>
+        <div className='info'>
+          <div className='more s-txt'>
+            <a href='#'>
+              Sobre BA
+            </a>
+            <a href='#'>
+              FAQ
+            </a>
+            <a href='#'>
+              Blog
+            </a>
+            <a href='#'>
+              Gift cards
+            </a>
+            <a href='#'>
+              Testimonials
+            </a>
+            <a href='#'>
+              Partners  
+            </a>
+            <a href='#'>
+              Unete a nosotros
+            </a>
+          </div>
+          <div className='locations'>
+            <h2>Locations</h2>
+              <p>San Juan</p>
+              <p>Carolina</p>
+              <p>Guaynabo</p>
+              <p>Canovanas</p>
+              <p>Caguas</p>
+              <p>Trujillo Alto</p>
+              <p>Bayamon</p>
+          </div>
+        </div>
       </div>
     ); 
   }
