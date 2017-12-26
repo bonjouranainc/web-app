bbdd1032cfdcaa221196363180450d517d0c64e7
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-12-25 23:17:53 -0400

    Header styling changed. Added menu icon on mobile instead of buttons

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import logo from './images/ba-logo-w.svg';
+
 import './styles.css';
 
 class Header extends Component {
@@ -9,11 +10,13 @@ class Header extends Component {
 
     this.state = {
       isHidden: false, 
-      headerColor: false
+      headerColor: false,
+      menuActive: false
     }
 
     this._hideHeader = this._hideHeader.bind(this);
     this._changeHeaderColor = this._changeHeaderColor.bind(this);
+    this._toggleMenu = this._toggleMenu.bind(this);
   }
 
   componentDidMount(){
@@ -31,7 +34,7 @@ class Header extends Component {
   let changeColor = this.state.headerColor ? 'var(--ba-yellow)' : 'transparent';
 
     return (
-      <div className={'header ' + classHide } 
+      <div className={ 'header ' + classHide } 
         style={{ backgroundColor: changeColor }}
       > 
         <div className='logo'>
@@ -39,14 +42,21 @@ class Header extends Component {
             <img src={ logo } alt='' />
           </a>
         </div>
-        <div className='menu s-txt'>
-          <a href='/' className='button-w-o'>
+        <div className='menu-icon'>
+          <button
+            onClick={ this._toggleMenu } 
+          >
+            <i className="material-icons">&#xE5D2;</i>
+          </button>
+        </div>
+        <div className='header-btns s-txt'>
+          <a href='https://bonjourana.com/collections/servicio-al-hogar' className='button-w-o'>
             Servicios
           </a>
-          <a href='/' className='button-w-o'>
+          <a href='https://bonjourana.wixsite.com/bonjourana/supermercado' className='button-w-o'>
             Supermercado
           </a>
-          <a href='/' className='button-w-o'>
+          <a href='https://bonjourana.com/account/login' className='button-w-o'>
             Sign in
           </a>
         </div>
@@ -70,6 +80,11 @@ class Header extends Component {
       :
       this.setState({ headerColor: false })
   }
+
+  _toggleMenu() {
+    this.setState({ menuActive: !this.state.menuActive });
+  }
+
 }
 
 export default Header;
