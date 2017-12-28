564758c7f8e3c1b370a3cdc7e8cf4e64dbf30ad8
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-12-28 11:12:11 -0400

    Unused routes disabled

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -6,8 +6,6 @@ import {
 } from 'react-router-dom';
 
 import Landing from './scenes/Landing/index.js';
-import About from './scenes/About/index.js'
-import LearnMore from './scenes/LearnMore/index.js'
 import TermsAndPrivacy from './scenes/TermsAndPrivacy/index.js'
 import Subscriptions from './scenes/Subscriptions/index.js'
 import Menu from './components/Menu/index.js'
@@ -18,8 +16,6 @@ class Routes extends Component {
       <Router> 
         <Switch>
           <Route exact path='/' component={Landing} />
-          <Route path='/about' component={About} />
-          <Route path='/learn' component={LearnMore} />
           <Route path='/terms' component={TermsAndPrivacy} />
           <Route path='/subscriptions' component={Subscriptions} />
           <Route path='/menu' component={Menu} />
