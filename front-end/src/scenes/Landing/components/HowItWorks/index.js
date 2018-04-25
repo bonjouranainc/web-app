a8a74653eb4e4ae07d81e18805c7828964e724ac
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-04-25 16:47:59 -0400

    Improved works section

--- front-end/src/scenes/Landing/components/HowItWorks/index.js
+++ front-end/src/scenes/Landing/components/HowItWorks/index.js
@@ -8,14 +8,14 @@ import './styles.css';
 
 const HowItWorks = () => (
   <div className="howItWorks wrapper">
-    <div>
-      <h1 className="b-txt">¿Cómo funciona?</h1>
+    <div className="headline">
+      <h2>¿Cómo funciona?</h2>
     </div>
     <div className="row">
       <div className="col group group1">
         <img src={url1} alt="image lady" className="image image-smaller" />
         <div className="cont">
-          <h2>Escoja sus servicios</h2>
+          <h3>Escoja sus servicios</h3>
           <p>
             Seleccione entre una variedad de servicios para el hogar y
             seleccione el día y hora que desea recibir el servicio
@@ -25,7 +25,7 @@ const HowItWorks = () => (
       <div className="col group group2">
         <img src={url2} alt="image cleaning" className="image image-bigger" />
         <div className="cont">
-          <h2>Reciba su visita</h2>
+          <h3>Reciba su visita</h3>
           <p>
             Un personal entrenado y confiable le visitará cada semana u ocasión,
             que le ofrecera un servicio de calidad
@@ -35,9 +35,9 @@ const HowItWorks = () => (
       <div className="col group group3">
         <img src={url3} alt="image chilling" className="image image-bigger" />
         <div className="cont">
-          <h2>
+          <h3>
             Voila! <br /> Todas sus tareas completadas
-          </h2>
+          </h3>
           <p>
             Llega a un hogar limpio y fresco, con las compras del supermercado
             realizada, el dry-cleaning recogido y todas sus tareas completadas.
