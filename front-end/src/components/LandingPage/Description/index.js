542f7e33bfbe028c6d0d7b72454bc115777220f8
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-06-02 14:31:31 -0400

    Finally made the  description component responsive. Have to work on the margins

--- front-end/src/components/LandingPage/Description/index.js
+++ front-end/src/components/LandingPage/Description/index.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import { Button, Grid, Container } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Button, Responsive, Grid } from 'semantic-ui-react';
 
 import './styles.css';
 
-const Description = () => (
-  <div className="description">
-    <div className="desc-box">
-      <h2 className="heading-primary">
-        <span className="heading-primary__main">
-          Enfócate en lo que <br />realmente importa.
-        </span>
-        <span className="heading-primary-sub">
-          Bonjour Ana es tu <br />asistente personal para el hogar y <br />necesidades
-          diarias.
-        </span>
-      </h2>
+export default class Description extends Component {
+  state = {};
 
-      <Button className="desc-box-button">
-        <a href="">Oferta de Limpieza</a>
-      </Button>
-    </div>
-  </div>
-);
+  handleUpdate = (e, { width }) => this.setState({ width });
 
-export default Description;
+  render() {
+    const { width } = this.state;
+    const textAlign =
+      width >= Responsive.onlyTablet.minWidth ? 'left' : 'center';
+
+    return (
+      <div className="description">
+        <Responsive
+          as={Grid}
+          columns={1}
+          fireOnMount
+          onUpdate={this.handleUpdate}
+        >
+          <Grid.Column textAlign={textAlign} className="desc-box">
+            <h2 className="heading-primary">
+              Enfócate en lo que realmente importa.
+            </h2>
+            <p className="heading-sub">
+              Bonjour Ana es tu asistente personal para el hogar y necesidades
+              diarias.
+            </p>
+            <Button className="desc-box-button">
+              <a href="">Oferta de Limpieza</a>
+            </Button>
+          </Grid.Column>
+        </Responsive>
+      </div>
+    );
+  }
+}
