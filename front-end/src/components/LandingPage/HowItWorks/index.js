6267b3d5fa1eabf9b7f9f899d4bcf53aeb70e0bb
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-06-15 06:33:57 -0400

    Finally managed and finished the howItWorks component. Fully responsive

--- front-end/src/components/LandingPage/HowItWorks/index.js
+++ front-end/src/components/LandingPage/HowItWorks/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Grid, Image, Container, Responsive } from 'semantic-ui-react';
 
 import url1 from '../../../images/png/Escoga los servicios.png';
 import url2 from '../../../images/png/Recibe tu Visita-07.png';
@@ -8,86 +7,54 @@ import url3 from '../../../images/png/Voila-07.png';
 import './styles.css';
 
 export default class HowItWorks extends Component {
-  state = {};
-
-  handleUpdate = (e, { width }) => this.setState({ width });
   render() {
-    const { width } = this.state;
-
-    const textAlignMiddle =
-      width >= Responsive.onlyTablet.minWidth ? 'right' : 'center';
-
-    const textAlign =
-      width >= Responsive.onlyTablet.minWidth ? 'justified' : 'center';
-
     return (
       <div className="howItWorks">
-        <Responsive
-          fireOnMount
-          onUpdate={this.handleUpdate}
-          className="howItWorks__grid"
-        >
-          <Grid>
-            <Grid.Row className="howItWorks__row">
-              <Grid.Column mobile={16} tablet={8} computer={8} padded="true">
-                <Image
-                  className="howItWorks__image"
-                  src={url1}
-                  size="medium"
-                  floated="right"
-                />
-              </Grid.Column>
-              <Grid.Column mobile={16} tablet={8} computer={8}>
-                <Container textAlign={textAlign}>
-                  <h3>Escoja sus servicios</h3>
-                  <p className="even-text">
-                    Seleccione entre una variedad de servicios para el hogar y
-                    seleccione el día y hora que desea recibir el servicio
-                  </p>
-                </Container>
-              </Grid.Column>
-            </Grid.Row>
-
-            <Grid.Row className="howItWorks__row" reversed="computer tablet">
-              <Grid.Column mobile={16} tablet={8} computer={8}>
-                <Image className="howItWorks__image" src={url2} size="large" />
-              </Grid.Column>
-              <Grid.Column mobile={16} tablet={8} computer={8}>
-                <Container
-                  className="howItWorks__text"
-                  textAlign={textAlignMiddle}
-                >
-                  <h3>Reciba su visita</h3>
-                  <p className="odd-text">
-                    Un personal entrenado y confiable le visitará cada semana u
-                    ocasión, que le ofrecera un servicio de calidad
-                  </p>
-                </Container>
-              </Grid.Column>
-            </Grid.Row>
-
-            <Grid.Row className="howItWorks__row">
-              <Grid.Column mobile={16} tablet={8} computer={8}>
-                <Image
-                  className="howItWorks__image"
-                  src={url3}
-                  size="large"
-                  floated="right"
-                />
-              </Grid.Column>
-              <Grid.Column mobile={16} tablet={8} computer={8}>
-                <Container className="howItWorks__text" textAlign={textAlign}>
-                  <h3>Voila! Todas sus tareas completadas</h3>
-                  <p className="even-text">
-                    Llega a un hogar limpio y fresco, con las compras del
-                    supermercado realizada, el dry-cleaning recogido y todas sus
-                    tareas completadas
-                  </p>
-                </Container>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Responsive>
+        <div className="groups">
+          {/* first content */}
+          <div className="group">
+            <div className="group-image">
+              <img className="first-image" src={url1} alt="" />
+            </div>
+            <div className="group-text group-text-1 group-text-even">
+              <h3>Escoja sus servicios</h3>
+              <p>
+                Seleccione entre una variedad de servicios para el hogar y
+                escoja el día y hora que desea recibir el servicvio.
+              </p>
+            </div>
+          </div>
+          {/* second content */}
+          <div className="group group-reverse">
+            <div className="group-image" id="group-image2">
+              <img src={url2} alt="" />
+            </div>
+            <div className="group-text group-text-odd">
+              <h3>Reciba su visita</h3>
+              <p>
+                Un personal entrenado y confiable le visitará cada semana u
+                ocasión para brindarle el servicio.
+              </p>
+            </div>
+          </div>
+          {/* third content */}
+          <div className=" group">
+            <div className="group-image">
+              <img src={url3} alt="" />
+            </div>
+            <div className="group-text group-text-even">
+              <h3>
+                Voila! <br />
+                Todas sus tareas completadas
+              </h3>
+              <p>
+                Llega a un hogar limpio y fresco, con las compras del
+                supermercado realizada, el dry-cleaning recogido y todas sus
+                tareas completadas.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
