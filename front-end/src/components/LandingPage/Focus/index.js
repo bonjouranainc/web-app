25da8e9deeed62b717e0012666791ac05e4c8fa6
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-06-17 12:02:25 -0400

    Modified slighty service component

--- front-end/src/components/LandingPage/Focus/index.js
+++ front-end/src/components/LandingPage/Focus/index.js
@@ -7,17 +7,21 @@ import picture from '../../../images/jpg/IMG_0247.JPG';
 
 const Focus = () => (
   <div className=".focus">
-    <div className="focus-column">
-      <h2 className="header-subtitle">Enfócate en lo que realmente importa.</h2>
-      <p className="paragraph">
-        Bonjour Ana es tu asistente personal para el hogar y necesidades
-        diarias.
-      </p>
-      <Button className="button-y">Oferta de limpieza</Button>
-    </div>
+    <div className="focus-row">
+      <div className="focus-column">
+        <h2 className="header-subtitle">
+          Enfócate en lo que realmente importa.
+        </h2>
+        <p className="paragraph">
+          Bonjour Ana es tu asistente personal para el hogar y necesidades
+          diarias.
+        </p>
+        <Button className="button-y">Oferta de limpieza</Button>
+      </div>
 
-    <div className="focus-image">
-      <img src={picture} alt="chilling" />
+      <div className="focus-image">
+        <img src={picture} alt="chilling" />
+      </div>
     </div>
   </div>
 );
--- front-end/src/components/LandingPage/Services/Service.js
+++ front-end/src/components/LandingPage/Services/Service.js
@@ -4,8 +4,8 @@ import { Card, Image } from 'semantic-ui-react';
 import './styles.css';
 
 const Service = props => (
-  <Card className="service-card" raised>
-    <Image src={props.icon} />
+  <Card raised>
+    <Image className="service-image" src={props.icon} />
     <Card.Content style={{ backgroundColor: '#f4f4f4' }}>
       <Card.Meta textAlign="center">{props.services}</Card.Meta>
     </Card.Content>
