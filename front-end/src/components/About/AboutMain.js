992e3f7e078d822eb4b9bd9075a2573b8983c4b1
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-06-27 13:04:02 -0400

    Implemented the about component, almost fully responsive

--- front-end/src/components/About/AboutMain.js
+++ front-end/src/components/About/AboutMain.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AboutGrid from './AboutGrid';
 import pic1 from '../../images/jpg/1.jpg';
 import pic4 from '../../images/jpg/2.jpg';
 import pic2 from '../../images/jpg/3.jpg';
@@ -15,35 +14,27 @@ const AboutMain = () => (
         diversidad de profesionales listos para ayudarte.
       </p>
     </div>
-    <div className="about__photo-grid">
-      <AboutGrid
-        picture={pic1}
-        name={'Valerie Santiago'}
-        profession={'Profesional de limpieza'}
-      />
 
-      <AboutGrid
-        picture={pic2}
-        name={'Edgardo Lopez'}
-        profession={'Profesional de limpieza'}
-      />
+    <div className="about__grid">
+      <div className="about__img-box">
+        <img src={pic1} alt="Profesional de limpieza" className="about__img" />
+      </div>
 
-      <AboutGrid
-        picture={pic3}
-        name={'Lee Sandra Meléndez'}
-        profession={'Profesional de limpieza'}
-      />
+      <div className="about__img-box">
+        <img src={pic2} alt="Profesional de limpieza" className="about__img" />
+      </div>
 
-      <AboutGrid
-        picture={pic4}
-        name={'Juan Negrón'}
-        profession={'Profesional de limpieza'}
-      />
-      <AboutGrid
-        picture={pic5}
-        name={'Alejandra Meléndez'}
-        profession={'Profesional de limpieza'}
-      />
+      <div className="about__img-box">
+        <img src={pic3} alt="Profesional de limpieza" className="about__img" />
+      </div>
+
+      <div className="about__img-box">
+        <img src={pic4} alt="Profesional de limpieza" className="about__img" />
+      </div>
+
+      <div className="about__img-box">
+        <img src={pic5} alt="Profesional de limpieza" className="about__img" />
+      </div>
     </div>
   </section>
 );
