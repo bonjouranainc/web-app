839c5e0c215553034b24c9ad88a78cc787c0136c
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-06-29 13:51:28 -0400

    Changed some misplaces variable names in files

--- front-end/src/components/TermsAndPrivacy/index.js
+++ front-end/src/components/TermsAndPrivacy/index.js
@@ -3,15 +3,10 @@ import React from 'react';
 import TermsOfUse from './TermsOfUse/index';
 import PrivacyPolicy from './PrivacyPolicy/index';
 
-import NavBar from '../LandingPage/NavBar/index';
-import Footer from '../LandingPage/Footer/index';
-
 const TermsAndPrivacy = () => (
   <div>
-    <NavBar />
     <TermsOfUse />
     <PrivacyPolicy />
-    <Footer />
   </div>
 );
 
