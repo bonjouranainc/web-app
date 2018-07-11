e28fec3588f6c2e0da467ca61e27fb6d0abeeb64
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-07-11 17:01:41 -0400

    changed routes to functional based component

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import LandingPage from '../components/LandingPage/index';
 import NavBar from '../components/NavBar';
@@ -9,23 +9,19 @@ import About from '../components/About/index';
 import TermsAndPrivacy from '../components/TermsAndPrivacy/index';
 import HowItWorks from '../components/HowItWorks/index';
 
-class Routes extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/terms" component={TermsAndPrivacy} />
-            <Route path="/about" component={About} />
-            <Route path="/how" component={HowItWorks} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-}
+const Routes = () => (
+  <BrowserRouter>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/terms" component={TermsAndPrivacy} />
+        <Route path="/about" component={About} />
+        <Route path="/how" component={HowItWorks} />
+      </Switch>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
 
 export default Routes;
