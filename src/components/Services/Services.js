39611d0826f8eccbf682d4a542609c7d5a6b83c6
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-07-20 14:42:52 -0400

    Added one more service

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,9 +3,11 @@ import Service from './Service';
 import service1 from '../../images/png/HandyMan.png';
 import service2 from '../../images/png/Plomeria.png';
 import service3 from '../../images/png/Electrisista.png';
+import service4 from '../../images/png/Ventanas.png';
 
 const Services = () => (
   <div className="services-page__grid">
+    <Service title={'Ventanas'} image={service4} />
     <Service title={'Handyman'} image={service1} />
     <Service title={'Plomería'} image={service2} />
     <Service title={'Electricista'} image={service3} />
