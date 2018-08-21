60cdd6b2b0cc16f1c57cb6ad6b206c4e49c4858c
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-08-21 11:29:25 -0400

    left note on future developer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import AppRouter from './routes/AppRouter';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/index.css';
 
+// To future developer: Don't push this to heroku
+// when you're done. Hide the domain and accessToken to another
+// file and don't show it on the web
 const domain = 'bonjour-ana.myshopify.com';
 const accessToken = '8079d4df4b84405bad995555bc06d537';
 
